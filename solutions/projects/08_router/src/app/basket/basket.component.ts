0a2d816e83c4d93422faead60f37cc554fa7ae4a
oumaimaBouchniba <oumaima.bouchniba@zenika.com>
2024-01-22 11:06:22 +0000

    Initial commit

---
+++ solutions/projects/08_router/src/app/basket/basket.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { BasketService } from './basket.service';
+
+@Component({
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+})
+export class BasketComponent {
+  protected get items() {
+    return this.basketService.items;
+  }
+
+  protected get total() {
+    return this.basketService.total;
+  }
+
+  constructor(private basketService: BasketService) {}
+}
